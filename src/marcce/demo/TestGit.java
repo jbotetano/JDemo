5dfd744a6a2ff0116c701ea3f1e0c0fd991d2f4c
jbotetan <jaime.botetano@gmail.com>
2013-08-17 23:12:43 +0200

    initial commit

---
+++ src/marcce/demo/TestGit.java
@@ -0,0 +1,14 @@
+package marcce.demo;
+
+public class TestGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is a test of Github");
+	}
+
+}
